d8d2f524234bfd30e090c3d88fb4fe23c9f8703c
Alphashow <lastblast@outlook.fr>
2022-12-18 17:44:42 +0100

    Changed GLAD to Core profile. Emptied main function.

--- LearningGLFW/main.cpp
+++ LearningGLFW/main.cpp
@@ -1,81 +1,9 @@
 #include <glad/glad.h>
 #include <glfw3.h>
 #include <iostream>
-#include "shader.hpp"
 
-void windowResize(GLFWwindow* window, int width, int height)
-{
-	glViewport(0, 0, width, height);
-}
 
 int main()
 {
-	if (!glfwInit()) {
-		std::cout << "Could not initialize glfw.\n";
-		return -1;
-	}
-
-	glfwWindowHint(GLFW_SAMPLES, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
-
-	GLFWwindow * window;
-	window = glfwCreateWindow(640, 480, "OpenGL TP 1", NULL, NULL);
-	if (!window)
-	{
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent(window);
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
-		std::cout << "Could not initialize GLAD\n";
-		exit(EXIT_FAILURE);
-	}
-
-	static constexpr float vertices[] = {
-		1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
-		0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f,
-		-1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
-	};
-	GLuint VAO;
-	GLuint VBO;
-	GLuint shaderProgram;
-
-	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
-
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)0);
-	glEnableVertexAttribArray(0);
-
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (void*)(sizeof(float) * 3));
-	glEnableVertexAttribArray(1);
-
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
-	shaderProgram = LoadShaders("../Shaders/SimpleVertexShader.vertexshader", "../Shaders/SimpleFragmentShader.fragmentshader");
-
-	glfwSetFramebufferSizeCallback(window, windowResize);
-
-
-	glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
-		while (!glfwWindowShouldClose(window))
-		{
-			glUseProgram(shaderProgram);
-			glBindVertexArray(VAO);	
-			glClear(GL_COLOR_BUFFER_BIT);
-
-			glDrawArrays(GL_TRIANGLES, 0, 3);
-
-			glfwSwapBuffers(window);
-			glfwPollEvents();
-		}
-	glfwDestroyWindow(window);
-	glfwTerminate();
+	
 }

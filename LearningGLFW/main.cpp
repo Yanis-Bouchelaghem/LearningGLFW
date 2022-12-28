4596c77395a8765266bddb19a586fc6a8d4572d3
Alphashow <lastblast@outlook.fr>
2022-12-28 12:56:27 +0100

    Used a uniform to set the color in the fragment shader.

--- LearningGLFW/main.cpp
+++ LearningGLFW/main.cpp
@@ -79,10 +79,11 @@ int main()
 
     //The fragment shader code
     const char* fragmentShaderSource = "#version 330 core\n"
+        "uniform vec4 vertexColor;"
         "out vec4 FragColor;\n"
         "void main()\n"
         "{\n"
-        "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+        "    FragColor = vertexColor;\n"
         "}";
     //Create a shader object to hold the fragment shader
     unsigned int fragmentShader;
@@ -162,6 +163,12 @@ int main()
         //Activate the shader program
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
+
+        // update the uniform color
+        float timeValue = glfwGetTime();
+        float greenValue = sin(timeValue) / 2.0f + 0.5f;
+        int vertexColorLocation = glGetUniformLocation(shaderProgram, "vertexColor");
+        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
         //glDrawArrays(GL_TRIANGLES, 0, 3);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
